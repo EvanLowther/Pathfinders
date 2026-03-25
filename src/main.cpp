e0c08271686c3bf2de9304525e09a1d35e6b4e2a
EvanLowther <evanlowther12@gmail.com>
2026-03-24 22:26:36 -0400

    Updated main.cpp so it reads the roadNet-PA.txt file, makes the adjacency list for calculations, and creates a JSON file for visualization.

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,13 @@
 #include "graph.h"
 using namespace std;
 
-int main(){
-    /*
-    This section is to test that the parser works
-    making sure that it reads the dataset properly
-    */
-
-    //Call parser to read the dataset
-    vector<pair<int,int>> edges = parseFile("../data/roadNet-PA.txt");
-    
-    //Total edge count (Should be 1541898 - undirected graph)
-    cout<< "Total edges: " << edges.size() << endl;
-    
-    //print out all Nodes and the list of its connections
+int main() {
+    //Read/analyze function for the roadNet-PA.txt file
+    vector<pair<int,int>> edges = parseFile("data/roadNet-PA.txt");
     Graph test(edges, 1090920);
-    test.printTestNodes(10);
 
-    //First 10 edges (functionality test)
-    for(int i = 0; i < 10; i++){
-        cout << edges[i].first << " -> " << edges[i].second << endl;
-    }
+    //Export roadNet-PA.txt to JSON (readble for JS) to make visualizer
+    test.exportJSON("data/road_network.json");
+
     return 0;
 }
