074c31eb42f2cde947a72bde2d693f51cf66c038
EvanLowther <evanlowther12@gmail.com>
2026-03-28 01:04:03 -0400

    Made it so User can Input two nodes to search between.

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,35 @@ using namespace std;
 int main() {
     //Read/analyze function for the roadNet-PA.txt file
     cout << "Parsing Pennsylvania road network:" << endl;
-    vector<pair<int,int>> edges = parseFile("data/roadNet-PA.txt"); // the road network file must be in the build directory
+    vector<pair<int,int>> edges = parseFile("../data/roadNet-PA.txt"); // the road network file must be in the build directory
     
     //Build the Graph
     //Total nodes in roadNet-PA.txt is  1,090,920
     int totalNodes = 1090920;
     Graph test(edges, totalNodes);
 
+
+    //Test parameters: feel free to change these IDs
+    int startNode;
+    int goalNode;
+    cout << "Enter Starting Node(0-1.9Mil): ";
+    cin >> startNode;
+    while(startNode < 0 || startNode >= totalNodes){
+        cout << "Invalid Number, Enter Again: ";
+        cin >> startNode;
+    }
+    cout << "Enter End/Goal Node: ";
+    cin >> goalNode;
+    while(goalNode < 0 || goalNode >= totalNodes){
+        cout << "Invalid Number, Enter Again: ";
+        cin >> goalNode;
+    }
+
     //Export for Visualizer
     test.exportJSON("../data/road_network.json");
     cout << "Updated Exported JSON File." << endl;
 
-    //Test parameters: feel free to change these IDs
-    int startNode = 10;
-    int goalNode = 500;
-    cout << "Searching for shortest path from " << startNode << " to " << goalNode << endl;
+    cout << "\nSearching for shortest path from " << startNode << " to " << goalNode << endl;
 
     //Run A* Pathfinding
     cout << "\n--- ASTAR ALGORITHM ---" << endl;
